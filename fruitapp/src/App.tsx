bd47dc611e1a145edc859dfc6d99e90a8a541c01
AlexBowWillCode <alexanderbowwill@gmail.com>
2024-02-11 15:04:21 +0000

    set up react query client

--- fruitapp/src/App.tsx
+++ fruitapp/src/App.tsx
@@ -1,7 +1,29 @@
+import { Grid, GridItem } from "@chakra-ui/react";
 import "./App.css";
+import { useState } from "react";
+
+export interface PeomQuery {
+  genreId?: number;
+  platformId?: number;
+  sortOrder: string;
+  searchText: string;
+}
 
 function App() {
-  return <></>;
+  const [poemQuery, setPoemQuery] = useState<PeomQuery>({} as PeomQuery);
+
+  return (
+    <Grid
+      templateAreas={{
+        base: `"main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+      }}
+    >
+      <GridItem area="main"></GridItem>
+    </Grid>
+  );
 }
 
 export default App;
